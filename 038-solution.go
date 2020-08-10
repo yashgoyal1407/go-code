ff99d99fdd575bc36a53ea43dad7d9ec714cf034
Yash Goyal <ygoyal@akamai.com>
2020-08-10 22:45:36 +0530

    solution 38

--- 038-solution.go
+++ 038-solution.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 )
 
-type whisky int
+type scotch int
 
-var x whisky
+var x scotch
+var y int
 
 func main() {
+	// part a
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+
+	// part b
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
