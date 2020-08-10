a2af1ea8a7ab9cf9804ab834294446ace17e41d4
Yash Goyal <ygoyal@akamai.com>
2020-08-11 00:41:04 +0530

    solution 048 - 053

---
+++ 050-solution.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 25
+	b = 40
+	d = 25
+)
+
+func main() {
+	fmt.Println(a==d)
+	fmt.Println(a<b)
+	fmt.Println(a>b)
+	fmt.Println(a<=b)
+	fmt.Println(a>=b)
+  fmt.Println(a!=d)
+}
